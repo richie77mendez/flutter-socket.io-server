b514e1d4aa93f61aee50948b5f9dad55ed105bd6
richie77mendez <augustoricardo-mendezpum@cunoc.edu.gt>
2021-08-14 23:10:57 -0600

    Fin seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,17 @@
+
+const {io} = require('../index');
+
+//mensajes de sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => { 
+        console.log('Cliente desconectado');
+     });
+
+    client.on('mensaje', (payload)=>{
+        console.log('mensaje!!!!', payload);
+
+        io.emit('mensaje', {admin: 'Nuevo mensaje'});
+    }); 
+  });
